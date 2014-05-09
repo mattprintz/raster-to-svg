c2a97d5c424c439cbf395ddb9df236b0f5f4a2bf
Matthew Printz <matt.printz@rackspace.com>
2014-05-09 15:12:22 -0500

    Hey look! A working prototype. Still much to do, though.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clojure-svg "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [
+      [org.clojure/clojure "1.5.1"]
+      [org.clojure/math.numeric-tower "0.0.4"]
+  ] 
   :main raster-to-svg.core)
--- src/raster_to_svg/core.clj
+++ src/raster_to_svg/core.clj
@@ -1,26 +1,84 @@
 (ns raster-to-svg.core
   (:import [javax.imageio ImageIO]
            [java.awt.image BufferedImage])
-  (:require [clojure.java.io]))
+  (:require [clojure.java.io])
+  (:require [clojure.math.numeric-tower :as math])
+)
 
 (defn get-pixels-for-block [raster block-x block-y block-size]
   (for [x (range (* block-x block-size) (* (inc block-x) block-size))
         y (range (* block-y block-size) (* (inc block-y) block-size))]
-    (.getPixel raster x y nil)))
+    (try 
+      (.getPixel raster x y nil)
+      (catch Exception e `(255 255 255 255))
+    )
+    ))
+
+(defn pixel-value [[r g b a]] ; Destructuring
+    (* (/ (+ r g b) 3) (if (nil? a) 1 (/ a 255.0)))
+ )
+
+(defn block-value [values]
+  (- 1 (/ (reduce + values) (count values) 255))
+)
 
 (defn process-block [raster block-x block-y block-size]
-  (get-pixels-for-block raster block-x, block-y, block-size))
+ (let [
+  block-mid (/ block-size 2)
+  pixels (get-pixels-for-block raster block-x, block-y, block-size)
+  pixel-values (for [pixel pixels] (pixel-value pixel))
+ ]
+  ;(print block-x " " block-y "\n")
+  {
+    :x (+ (* block-x block-size) block-mid)
+    :y (+ (* block-y block-size) block-mid)
+    :value (block-value pixel-values)
+    :block-size block-size
+  }
+ )
+)
 
-(defn process-image []
-  (let [block-size 8
-        filename "resources/candle.png"
+(defn svg-circle [{x :x
+                   y :y
+                   value :value
+                   block-size :block-size}]
+  (let [
+    radius (* value (/ block-size 2))
+  ]
+    (format "<circle cx=\"%d\" cy=\"%d\" r=\"%f\" />" x y (float radius))
+  )
+)
+
+(defn build-svg [svg-fn block-values]
+  (str
+    "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<svg baseProfile=\"tiny\" version=\"1.2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:ev=\"http://www.w3.org/2001/xml-events\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"><defs />"
+    (apply str (map svg-fn (filter (fn [a] (> (:value a))) block-values)))
+    ;(apply str (pmap svg-fn (filter (fn [a] (> (:value a))) block-values)))
+    ;(apply str (for [block block-values :when (> (:value block) 0.1)] (svg-fn block)))
+    "</svg>"
+  )
+)
+
+(defn save-svg [data-string]
+  (spit "/home/mprintz/test.svg" data-string)
+)
+
+(defn process-image [filename]
+  (let [
+        block-size 8
         imagefile (clojure.java.io/as-file filename)
         raster (.getData (. ImageIO read imagefile))
-        width-in-blocks (/ (.getWidth raster) block-size)
-        height-in-blocks (/ (.getHeight raster) block-size)           
-        block-coords (for [x (range width-in-blocks) y (range height-in-blocks)] [x y])]
-
-    (map (fn [[x y]] (process-block raster x y block-size)) block-coords)))
+        width-in-blocks (math/ceil (/ (.getWidth raster) block-size))
+        height-in-blocks (math/ceil (/ (.getHeight raster) block-size))
+        block-coords (for [x (range width-in-blocks) y (range height-in-blocks)] [x y])
+        values (map (fn [[x y]] (process-block raster x y block-size)) block-coords)
+        svg-data (build-svg svg-circle values)
+      ]
+      
+      (save-svg svg-data)
+    )
+  )
 
 (defn -main [& args]
-  (println (process-image)))
+  (process-image (first args))
+)
